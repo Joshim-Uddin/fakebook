09a550955868cf6c3d4005d17ae1c1fdf8565d77
Joshim-Uddin <mju881@gmail.com>
2024-02-18 22:44:20 +0600

    navbar updated

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,69 +1,74 @@
+'use client'
 import logo from "@/assets/img/logo.png";
 import Image from 'next/image';
 import Link from 'next/link';
 import { RiHome4Fill } from "react-icons/ri";
-import { MdOutlineOndemandVideo, MdNotifications } from "react-icons/md";
+import { MdOutlineOndemandVideo, MdNotifications, MdSearch } from "react-icons/md";
 import { FaStore, FaGamepad, FaFacebookMessenger} from "react-icons/fa6";
 import { HiUserGroup } from "react-icons/hi2";
 import { PiDotsNineBold } from "react-icons/pi";
+import { useContext } from "react";
+import { fakebookContext } from "@/app/contextApi";
 
 const Navbar =()=>{
+    const {user} = useContext(fakebookContext)
     return (
-        <nav>
-            <div id='fakebook' className='flex justify-center items-center'>
-            <div className='w-10 h-10'>
+        <nav className="grid gap-5 grid-cols-4 bg-white border-b shadow-sm min-h-16 fixed top-0 left-0 right-0 px-10">
+            <div id='fakebook' className='flex justify-center items-center gap-4 mr-8'>
+            <div>
                 <Link href='/home'><Image src={logo} alt='fakebook logo' width={200} height={200}></Image></Link>
             </div>
-            <div>
-                <input type='text' placeholder='Search' className='p-2 border rounded-3xl'></input>
+            <div className="relative">
+                <input type='text' placeholder='Search' className='p-2 focus:outline-none border rounded-3xl ps-3'>
+                </input>
             </div>
             </div>
-            <div id='menubar'>
+            <div id='menubar' className="flex gap-5 justify-around items-center col-span-2 mr-10">
             <div>
-                <button>
-                    <Link href='/'><RiHome4Fill /></Link>
+                <button className="p-2 hover:bg-gray-200 hover:rounded-full">
+                    <Link href='/'><RiHome4Fill className="text-2xl text-blue-600"/></Link>
                 </button>
             </div>
             <div>
-            <button>
-                    <Link href='/'><MdOutlineOndemandVideo /></Link>
+            <button className="p-2 hover:bg-gray-200 hover:rounded-full">
+                    <Link href='/'><MdOutlineOndemandVideo className="text-2xl text-blue-600"/></Link>
                 </button>
             </div>
             <div>
-            <button>
-                    <Link href='/'><FaStore /></Link>
+            <button className="p-2 hover:bg-gray-200 hover:rounded-full">
+                    <Link href='/'><FaStore className="text-2xl text-blue-600"/></Link>
                 </button>
             </div>
             <div>
-            <button>
-                    <Link href='/'><HiUserGroup /></Link>
+            <button className="p-2 hover:bg-gray-200 hover:rounded-full">
+                    <Link href='/'><HiUserGroup className="text-2xl text-blue-600"/></Link>
                 </button>
             </div>
             <div>
-            <button>
-                    <Link href='/'><FaGamepad /></Link>
+            <button className="p-2 hover:bg-gray-200 hover:rounded-full">
+                    <Link href='/'><FaGamepad className="text-2xl text-blue-600"/></Link>
                 </button>
             </div>
             
             </div>
-            <div id='profile'>
+            <div id='profile' className="flex gap-5 justify-around items-center">
             <div>
-            <button>
-                    <Link href='/'><PiDotsNineBold /></Link>
+            <button className="p-2 hover:bg-gray-200 hover:rounded-full">
+                    <Link href='/'><PiDotsNineBold className="text-2xl text-blue-600"/></Link>
                 </button>
             </div>
             <div>
-            <button>
-                    <Link href='/'><FaFacebookMessenger /></Link>
+            <button className="p-2 hover:bg-gray-200 hover:rounded-full">
+                    <Link href='/'><FaFacebookMessenger className="text-2xl text-blue-600"/></Link>
                 </button>
             </div>
             <div>
-            <button>
-                    <Link href='/'><MdNotifications /></Link>
+            <button className="p-2 hover:bg-gray-200 hover:rounded-full">
+                    <Link href='/'><MdNotifications className="text-2xl text-blue-600"/></Link>
                 </button>
             </div>
             <div>
-
+                <h2>{user}</h2>
             </div>
             </div>
 
